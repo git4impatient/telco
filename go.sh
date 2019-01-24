b05047e4f62eb4a304f7b31037890ec13672dd4e
git4impatient <lurie@lurie.biz>
2019-01-23 19:32:56 -0500

    Update go.sh

--- go.sh
+++ go.sh
@@ -2,4 +2,6 @@
  hadoop fs -mkdir cdranomaly
  hadoop fs -rm cdranomaly/cdr.dat 
  hadoop fs -put cdr.dat cdranomaly
- impala-shell -f cdranomaly.ddl 
+ export DATANODE=$(sudo su - hdfs -c "hdfs dfsadmin -report | grep Hostname | sed 's/.*: //'  | tail -1")
+echo $DATANODE
+ impala-shell -i $DATANODE -f cdranomaly.ddl 
